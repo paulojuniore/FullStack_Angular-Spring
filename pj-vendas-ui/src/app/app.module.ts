63ea48b3b7b9e198711bd90550861c37bf5be8df
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-01-22 23:41:41 -0300

    Instalando e configurando o PrimeNG e PrimeIcons no projeto.

--- pj-vendas-ui/src/app/app.module.ts
+++ pj-vendas-ui/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { VendasListagemComponent } from './vendas-listagem/vendas-listagem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    VendasListagemComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
